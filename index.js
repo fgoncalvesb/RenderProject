d7c0037fbef97bddc5c9ae78e389d78238d1ab94
fgoncalvesb <kundogb@gmail.com>
2023-03-04 18:46:47 +0100

    More changes

--- index.js
+++ index.js
@@ -1,68 +1,11 @@
-var express = require('express');
-var fs = require('fs');
-var app = express();
-
-// Require the module required for using form data
-var bodyParser = require('body-parser');
-
-// For parsing application
-app.use(bodyParser.urlencoded({ extended: true}))
-
-// Homepage
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/Project1/public/index.html');
-})
-
-app.get('/guestbook', function (req, res) {
-    var data = require('./jsonguestbookdata.json');
-    //res.json(data);
-
-    var results = '<table border="1"> ';
-
-    for (var i = 0 ; i < data.length ; i++ ){
-        results +=
-        '<tr>' +
-        '<td>' + data[i].username + '</td>' +
-        '<td>' + data[i].country + '</td>' +
-        '<td>' + data[i].date + '</td>' +
-        '<td>' + data[i].message + '</td>' +
-        '</tr>';
-    }
-
-    res.send(results);
-
-});
-
-//Route for form sending the POST data
-app.post('/adduser', function (req, res) {
-
-    var data = require('./exampledata2.json');
-
-    data.push({
-    "Name": req.body.name,
-    "Company": req.body.company,
-    "Email": req.body.email,
-    "Date": new Date()
-
-    })
-
-    // Convert the JSON object to a string format
-    var jsonStr = JSON.stringify(data);
-
-    // Wrote data to a file
-    fs.writeFile('exampledata2.json', jsonStr, (err) => {
-        if (err) throw err;
-        console.log ('it\'s saved!');
-    });
-    
-    res.send("Saved the data to a file. Browse to the /details to see the content of the file")
-});
-
-//The 404 Route (Always kepe this as the last route)
-app.get('*', function (req, res){
-    res.status(404).send('Cant find the requested page');
-});
-
-app.listen(8080, function (){
-    console.log('Example app listing on port 8080!')
-});
+const PORT = process.env.PORT || 3000; 
+var http = require("http"); 
+ 
+//create a server object: 
+http 
+  .createServer(function(request, response) { 
+    response.writeHead(200, { "Content-Type": "text/plain" }); 
+    response.write("Hello World!\n"); //write a response to the client 
+    response.end("This is the end"); //end the response 
+  }) 
+  .listen(PORT); //the server object listens on port 8080 
